ff6822ef7c952904f75c27d24f1d77157cd236ba
Haroldo Cruz <haroldocruzdr@gmail.com>
2024-02-10 21:20:57 -0300

    refactor(fixture): imports

--- src/fixture.ts
+++ src/fixture.ts
@@ -1,19 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DataSource } from 'typeorm';
 
 import { UtilRepository } from '@libs/common/repository/util.repository';
+import { dbPgPilotoConfig_fixture } from '@sd-root/libs/common/src/databases/db-pg-piloto.config';
 import { AppModule } from './app.module';
 import usuarios from './fixtures/usuarios';
 import { ContatoEntity } from './usuario/models/entities/contato.entity';
+import { DataAccessEntity } from './usuario/models/entities/data-access.entity';
 import { EmailEntity } from './usuario/models/entities/email.entity';
 import { EnderecoEntity } from './usuario/models/entities/endereco.entity';
+import { LoginInfoEntity } from './usuario/models/entities/login-info.entity';
+import { ProfileEntity } from './usuario/models/entities/profile.entity';
 import { TelefoneEntity } from './usuario/models/entities/telefone.entity';
 import { UsuarioEntity } from './usuario/models/entities/usuario.entity';
-import { LoginInfoEntity } from './usuario/models/entities/login-info.entity';
-import { DataAccessEntity } from './usuario/models/entities/data-access.entity';
-import { ProfileEntity } from './usuario/models/entities/profile.entity';
-import configs from '@libs/common/configs';
-import { DataSource } from 'typeorm';
-import { dbPgPilotoConfig_fixture } from '@sd-root/libs/common/src/databases/db-pg-piloto.config';
 
 async function bootstrap() {
 
